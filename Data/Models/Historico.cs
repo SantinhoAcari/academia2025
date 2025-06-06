6ce7bf7309a95085cd6c9f4fcdbd082112d51934
Antônio Garcia <antonio.garciamn@gmail.com>
2025-06-06 20:54:14 -0300

    banco de dados inciail

---
+++ Data/Models/Historico.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academia2025.Models
+{
+    public class Historico
+    {
+        [Key]
+        public int IdHistorico { get; set; }
+
+        [Required]
+        public int FkIdMembro { get; set; }
+
+        [ForeignKey("FkIdMembro")]
+        public required Membro Membro { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public required string Atividade { get; set; }
+
+        [Required]
+        public DateTime DataAtividade { get; set; }
+
+        [Required]
+        public int TempoExecucao { get; set; }
+
+        [Required]
+        public int FkIdFuncionario { get; set; }
+
+        [ForeignKey("FkIdFuncionario")]
+        public required Funcionario Funcionario { get; set; }
+    }
+}
---
+++ Data/Models/Treino.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academia2025.Models
+{
+    public class Treino
+    {
+        [Key]
+        public int IdTreino { get; set; }
+
+        [Required]
+        public int FkIdMembro { get; set; }
+
+        [ForeignKey(nameof(FkIdMembro))]
+        public required Membro Membro { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Tipo { get; set; }
+
+        [StringLength(200)]
+        public string? Descricao { get; set; }
+
+        [Required]
+        public DateTime DataInicio { get; set; }
+    }
+}
